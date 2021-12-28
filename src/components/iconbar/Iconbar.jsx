fef5ec53a4de4d61cf6e8591fb6fc9cd4b0780ed
AshishSingh <as73653@gmail.com>
2021-12-29 01:29:12 +0530

    Completed hero page

--- src/components/iconbar/Iconbar.jsx
+++ src/components/iconbar/Iconbar.jsx
@@ -13,13 +13,13 @@ function Iconbar() {
     return (
         <div>
             <div className="icon-container">
-            <Link to="/" style={{ textDecoration: 'none', color: 'white' }}><HomeIcon style={{padding:'8px 0',fontSize:'32',cursor:'pointer'}}/></Link>
-            <Link to="/About" style={{ textDecoration: 'none', color: 'white' }}><InfoIcon style={{padding:'8px 0',fontSize:'32',cursor:'pointer'}}/></Link>
-            <Link to="/Certification" style={{ textDecoration: 'none', color: 'white' }}><DescriptionIcon style={{padding:'8px 0',fontSize:'32',cursor:'pointer'}}/></Link>
-            <Link to="/Projects" style={{ textDecoration: 'none', color: 'white' }}><WbIncandescentIcon style={{padding:'8px 0',fontSize:'32',cursor:'pointer'}}/></Link>
-            <Link to="/Skills" style={{ textDecoration: 'none', color: 'white' }}><SettingsIcon style={{padding:'8px 0',fontSize:'32',cursor:'pointer'}}/></Link>
-            <Link to="/Photowall" style={{ textDecoration: 'none', color: 'white' }}><PhotoIcon style={{padding:'8px 0',fontSize:'32',cursor:'pointer'}}/></Link>
-            <Link to="/Connect" style={{ textDecoration: 'none', color: 'white' }}><LinkIcon style={{padding:'8px 0',fontSize:'32',cursor:'pointer'}}/></Link>
+            <Link to="/" style={{ textDecoration: 'none', color: 'white' }}><HomeIcon style={{padding:'8px 0',fontSize:'42',cursor:'pointer'}}/></Link>
+            <Link to="/About" style={{ textDecoration: 'none', color: 'white' }}><InfoIcon style={{padding:'8px 0',fontSize:'42',cursor:'pointer'}}/></Link>
+            <Link to="/Certification" style={{ textDecoration: 'none', color: 'white' }}><DescriptionIcon style={{padding:'8px 0',fontSize:'42',cursor:'pointer'}}/></Link>
+            <Link to="/Projects" style={{ textDecoration: 'none', color: 'white' }}><WbIncandescentIcon style={{padding:'8px 0',fontSize:'42',cursor:'pointer'}}/></Link>
+            <Link to="/Skills" style={{ textDecoration: 'none', color: 'white' }}><SettingsIcon style={{padding:'8px 0',fontSize:'42',cursor:'pointer'}}/></Link>
+            <Link to="/Photowall" style={{ textDecoration: 'none', color: 'white' }}><PhotoIcon style={{padding:'8px 0',fontSize:'42',cursor:'pointer'}}/></Link>
+            <Link to="/Connect" style={{ textDecoration: 'none', color: 'white' }}><LinkIcon style={{padding:'8px 0',fontSize:'42',cursor:'pointer'}}/></Link>
             </div>
         </div>
     )
--- src/components/main/hero/Hero.jsx
+++ src/components/main/hero/Hero.jsx
@@ -1,12 +1,31 @@
-import React from 'react';
+import React from "react";
 import "./hero.css";
 
 function Hero() {
-    return (
-        <div>
-            <h1>Hero</h1>
-        </div>
-    )
+  return (
+    <div className="back">
+      <div className="hero flex items-center justify-between" id="home">
+                    <div className="left flex-1 flex justify-center">
+                        <img src="./images/man.png" alt="man"/>
+                    </div>
+                    <div className="right flex-1">
+                        <h6>Ashish Singh</h6>
+                        <h1>I'm an Engineering <span>Student</span></h1>
+                        <p>My three major interests and passions are web development , programming and Music, and I
+                            believe
+                            that there is a creative fusion between all these disciplines. I engage wholeheartedly in
+                            these
+                            areas both in my school courses and out of school, and hope that I will be able to continue
+                            doing so on my chosen course and in the extra-curricular opportunities at university..</p>
+                        <div>
+                            <a href="./images/Resume/AshishSingh_1902023_CSE.docx" download><button
+                                    className="btn btn-secondary">
+                                    RESUME</button></a>
+                        </div>
+                    </div>
+                </div>
+    </div>
+  );
 }
 
-export default Hero
+export default Hero;
